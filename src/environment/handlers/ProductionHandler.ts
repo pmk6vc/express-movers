1ea7ce19c2123497e51fd4e3083d2f9c686b8a8e
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-06-29 17:30:55 -0400

    add console logging

--- src/environment/handlers/ProductionHandler.ts
+++ src/environment/handlers/ProductionHandler.ts
@@ -5,7 +5,8 @@ export class ProductionHandler extends AbstractHandler {
   async runMigration() {
     const env = await this.getEnvironment();
     process.env.DATABASE_URL = env.database.getConnectionString();
-    await exec(`npx prisma migrate deploy`, {
+    console.log(`Running migrate command with this DATABASE_URL: ${process.env.DATABASE_URL}`);
+    await exec(`prisma migrate deploy`, {
       env: process.env,
     });
   }

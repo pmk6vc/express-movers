68281c23e4d5033988f4cdb979b1880fe3c61b53
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-08-10 16:14:18 -0400

    get docker compose working

--- src/environment/handlers/LocalDevHandler.ts
+++ src/environment/handlers/LocalDevHandler.ts
@@ -1,8 +1,17 @@
 import AbstractHandler from "./AbstractHandler";
 import { PostgreSqlContainer } from "@testcontainers/postgresql";
 import PostgresConfig from "../util/PostgresConfig";
+import run from "node-pg-migrate";
 
 export class LocalDevHandler extends AbstractHandler {
+  protected override async getServerConfig() {
+    if (this.serverConfig == undefined) {
+      this.serverConfig = {
+        serverPort: 5495,
+      };
+    }
+    return this.serverConfig;
+  }
   protected override async getDatabaseConfig() {
     if (this.databaseConfig == undefined) {
       const container = await new PostgreSqlContainer().start();
@@ -19,5 +28,11 @@ export class LocalDevHandler extends AbstractHandler {
   async runMigration() {
     const env = await this.getEnvironment();
     console.log("You've reached the local dev handler migration!");
+    await run({
+      migrationsTable: "pgmigrations",
+      dir: "/app/migrations",
+      direction: "up",
+      databaseUrl: env.database.getConnectionString(),
+    });
   }
 }

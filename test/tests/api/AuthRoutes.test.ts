ff746397e3d81831bb0c57fb571bd7a0df82c64b
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-08-24 23:05:54 -0400

    first passing state for firebase emulator testing

---
+++ test/tests/api/AuthRoutes.test.ts
@@ -0,0 +1,58 @@
+import { beforeEach, describe, expect, it } from "@jest/globals";
+import { Express } from "express";
+import EnvironmentResolver from "../../../src/environment/EnvironmentResolver";
+import { buildApp } from "../../../src/app";
+import request from "supertest";
+import { getAuth } from "firebase-admin/auth";
+import * as admin from "firebase-admin";
+
+// TODO: Requires emulator to be running at this IP address and GCP project
+// TODO: firebase emulators:start --only auth --project test
+// To test signed in user, see: https://www.reddit.com/r/Firebase/comments/qmsr9h/writeup_on_testing_cloud_functions_with_the/
+process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099";
+admin.initializeApp({
+  projectId: "test",
+});
+
+describe("should check auth routes", () => {
+  let app: Express;
+  let uid: string;
+  const ENV = process.env;
+
+  beforeAll(async () => {
+    const env = await EnvironmentResolver.getEnvironment();
+    app = buildApp(env);
+    const user = await getAuth().createUser({
+      email: "user@example.com",
+      emailVerified: false,
+      phoneNumber: "+11234567890",
+      password: "secretPassword",
+      displayName: "John Doe",
+      photoURL: "http://www.example.com/12345678/photo.png",
+      disabled: false,
+    });
+    uid = user.uid;
+  });
+
+  beforeEach(async () => {
+    jest.resetModules();
+    process.env = { ...ENV };
+    await EnvironmentResolver.getEnvironmentHandler().runUpMigrations();
+  });
+
+  afterEach(async () => {
+    await EnvironmentResolver.getEnvironmentHandler().runDownMigrations();
+  });
+
+  afterAll(() => {
+    process.env = ENV;
+    getAuth().deleteUser(uid);
+  });
+
+  const ROUTE_PREFIX = "/auth";
+
+  it("returns a list of users", async () => {
+    const res = await request(app).get(`${ROUTE_PREFIX}/list`).expect(200);
+    console.log(res.body);
+  });
+});

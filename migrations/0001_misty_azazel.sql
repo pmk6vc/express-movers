9eb6ecb87880de96055653a321031c0d803c2cda
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-09-05 20:44:47 -0400

    consolidate migrations

--- migrations/0001_misty_azazel.sql
+++ migrations/0001_misty_azazel.sql
@@ -31,13 +31,13 @@ CREATE TABLE IF NOT EXISTS "auth"."roles_permissions" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "auth"."roles_permissions" ADD CONSTRAINT "roles_permissions_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "auth"."roles"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "auth"."roles_permissions" ADD CONSTRAINT "roles_permissions_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "auth"."roles"("id") ON DELETE restrict ON UPDATE cascade;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "auth"."roles_permissions" ADD CONSTRAINT "roles_permissions_permission_id_permissions_id_fk" FOREIGN KEY ("permission_id") REFERENCES "auth"."permissions"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "auth"."roles_permissions" ADD CONSTRAINT "roles_permissions_permission_id_permissions_id_fk" FOREIGN KEY ("permission_id") REFERENCES "auth"."permissions"("id") ON DELETE restrict ON UPDATE cascade;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;

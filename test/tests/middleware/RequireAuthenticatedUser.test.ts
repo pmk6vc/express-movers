47accea5add31e14eb3ac8953a0ff52451fc4c60
Varun Kulkarni <varunkulkarni@gmail.com>
2023-11-08 23:00:40 -0500

    add test cases

---
+++ test/tests/middleware/RequireAuthenticatedUser.test.ts
@@ -0,0 +1,39 @@
+import { afterEach, beforeEach, describe } from "@jest/globals";
+import { NextFunction, Request, Response } from "express";
+import { USER_PROPERTY } from "../../../src/middleware/AuthenticateUser";
+import requireAuthenticatedUser from "../../../src/middleware/RequireAuthenticatedUser";
+
+describe("require authentication middleware should work", () => {
+  const mockRequest: Request = {} as Request;
+  let mockResponse: Response;
+  const nextFunction: NextFunction = jest.fn();
+
+  beforeEach(() => {
+    mockResponse = {
+      locals: {},
+    } as Response;
+    mockResponse.status = jest.fn(() => mockResponse);
+    mockResponse.send = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should block request without authenticated user", async () => {
+    await requireAuthenticatedUser(mockRequest, mockResponse, nextFunction);
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+    expect(mockResponse.status).toHaveBeenCalledTimes(1);
+    expect(mockResponse.send).toHaveBeenCalledTimes(1);
+    expect(nextFunction).toHaveBeenCalledTimes(0);
+  });
+
+  it("should allow request with authenticated user", async () => {
+    const USER_PROPERTY_KEY = USER_PROPERTY as keyof typeof mockResponse.locals;
+    mockResponse.locals[USER_PROPERTY_KEY] = "Some authenticated user data";
+    await requireAuthenticatedUser(mockRequest, mockResponse, nextFunction);
+    expect(mockResponse.status).toHaveBeenCalledTimes(0);
+    expect(mockResponse.send).toHaveBeenCalledTimes(0);
+    expect(nextFunction).toHaveBeenCalledTimes(1);
+  });
+});

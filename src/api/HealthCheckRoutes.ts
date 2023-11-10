78b4c974034d24ce6d437a67fe9f917581085bee
Varun Kulkarni <varunkulkarni@gmail.com>
2023-11-09 22:31:46 -0500

    add metadata to health check log

--- src/api/HealthCheckRoutes.ts
+++ src/api/HealthCheckRoutes.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import { Logger } from "winston";
 import DatabaseClient from "../db/DatabaseClient";
+import { GLOBAL_LOG_OBJ } from "../middleware/CorrelatedRequestLogging";
 
 const router = express.Router();
 
 const healthCheckRouter = (dbClient: DatabaseClient, logger: Logger) => {
   return router.get("/", (req, res) => {
-    logger.info("You made it to the health check endpoint!");
+    logger.info(
+      "You made it to the health check endpoint!",
+      res.locals[GLOBAL_LOG_OBJ]
+    );
     res.send("Hello, world!");
   });
 };

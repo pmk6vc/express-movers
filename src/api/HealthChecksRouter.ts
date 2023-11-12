acca056f1fb5f630b79fba4269dfbe258fc1c5fd
Varun Kulkarni <varunkulkarni@gmail.com>
2023-11-11 21:06:30 -0500

    use ping endpoint instead

--- src/api/HealthChecksRouter.ts
+++ src/api/HealthChecksRouter.ts
@@ -3,14 +3,11 @@ import { GLOBAL_LOG_OBJ } from "../middleware/CorrelatedRequestLogging";
 import AbstractRouter from "./AbstractRouter";
 
 export default class HealthChecksRouter extends AbstractRouter {
-  private helloWorld = async (req: Request, res: Response) => {
-    this.logger.info(
-      "Hello from the health check endpoint!",
-      res.locals[GLOBAL_LOG_OBJ]
-    );
-    res.send("Hello, world!");
+  private ping = async (req: Request, res: Response) => {
+    this.logger.info("You pinged me!", res.locals[GLOBAL_LOG_OBJ]);
+    res.send("Ping!");
   };
   buildRouter(): Router {
-    return express.Router().get("/", this.helloWorld);
+    return express.Router().get("/ping", this.ping);
   }
 }

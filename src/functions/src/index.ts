de4b43f0c0c07eab6934491a075554a861152046
Varun Kulkarni <varunkulkarni@gmail.com>
2023-12-03 19:26:26 -0500

    add log statement

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -5,6 +5,7 @@ import * as functions from "firebase-functions/v1";
 export const newUser = functions.auth
   .user()
   .onCreate(async (userRecord: UserRecord) => {
+    console.log("Hello from Firebase function!");
     await axios.post("http://localhost:5495/users/writeNewUser", {
       uid: userRecord.uid,
     });

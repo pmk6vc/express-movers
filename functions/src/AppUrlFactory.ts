9e64169be5b9f55b3f9bdae0d1283b461846dfe5
Varun Kulkarni <varunkulkarni@gmail.com>
2023-12-03 22:41:32 -0500

    commiting stuff that works so far?

--- functions/src/AppUrlFactory.ts
+++ functions/src/AppUrlFactory.ts
@@ -1,11 +1,11 @@
-export default class UrlFactory {
+export default class AppUrlFactory {
   private static urlMap = new Map<string, string>([
     ["local-dev", "http://localhost:5495"],
-    ["local-docker-compose", "http://app:5495"],
+    ["local-docker-compose", `http://app:${process.env.APP_PORT}`],
     ["production", "https://express-movers-h3fpwbsj7a-uc.a.run.app"],
   ]);
 
   static getUrl(): string {
-    return UrlFactory.urlMap.get(process.env.CONFIG_ENV || "local-dev")!;
+    return AppUrlFactory.urlMap.get(process.env.CONFIG_ENV || "local-dev")!;
   }
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as admin from "firebase-admin";
 import { UserRecord } from "firebase-admin/auth";
 import * as functions from "firebase-functions/v1";
-import UrlFactory from "./UrlFactory";
+import AppUrlFactory from "./AppUrlFactory";
 
 admin.initializeApp({
   projectId: process.env.GCP_PROJECT_ID,
@@ -10,7 +10,8 @@ admin.initializeApp({
 export const newUser = functions.auth
   .user()
   .onCreate(async (userRecord: UserRecord) => {
-    await axios.post(`${UrlFactory.getUrl()}/users/writeNewUser`, {
+    // TODO: Authenticate as service account and add ID token to request
+    await axios.post(`${AppUrlFactory.getUrl()}/users/writeNewUser`, {
       uid: userRecord.uid,
     });
   });

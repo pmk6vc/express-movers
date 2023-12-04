476eadcca16bad07a9a6c52ec103b7eabd76dec5
Varun Kulkarni <varunkulkarni@gmail.com>
2023-12-03 19:36:55 -0500

    move functions to top level and add admin initialization

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
+import * as admin from "firebase-admin";
 import { UserRecord } from "firebase-admin/auth";
 import * as functions from "firebase-functions/v1";
 
+admin.initializeApp({
+  projectId: process.env.GCP_PROJECT_ID,
+});
 export const newUser = functions.auth
   .user()
   .onCreate(async (userRecord: UserRecord) => {

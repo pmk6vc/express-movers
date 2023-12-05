52de3fc1289dcd9b1ad3659ad12cd65c305460de
Varun Kulkarni <varunkulkarni@gmail.com>
2023-12-04 19:00:22 -0500

    fix name of deployed function

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -3,11 +3,11 @@
 gcloud functions deploy newUserFuncManualDeploy \
 --runtime=nodejs18 \
 --region=us-central1 \
---entrypoint=newUser \
---set-env-vars=[CONFIG_ENV=production]
---max-instances=5
---project=zugzwang-381922
---memory=128MB
---trigger-event=providers/firebase.auth/eventTypes/user.create
+--entry-point=newUser \
+--set-env-vars=CONFIG_ENV=production,GCLOUD_PROJECT=zugzwang-381922 \
+--max-instances=5 \
+--project=zugzwang-381922 \
+--memory=128MB \
+--trigger-event=providers/firebase.auth/eventTypes/user.create \
 --trigger-resource=zugzwang-381922
 # --service-account # TODO

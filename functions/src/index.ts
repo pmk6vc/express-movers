ac72bd04a123a46b9ea94f7269d1f30db930df52
Varun Kulkarni <varunkulkarni@gmail.com>
2023-12-04 23:17:45 -0500

    add retry logic

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,12 +10,44 @@ admin.initializeApp({
 export const persistNewFirebaseUser = functions.auth
   .user()
   .onCreate(async (userRecord: UserRecord) => {
-    // TODO: Logging and error handling
     // TODO: Test coverage
-    // TODO: Set up retries and add termination condition to avoid infinite retries (either in code or in deployment)
     // TODO: Add delete user equivalent
     // TODO: Authenticate as service account and add ID token to request
-    await axios.post(`${AppUrlFactory.getUrl()}/users/writeNewUser`, {
-      uid: userRecord.uid,
-    });
+
+    // Set up time-based termination condition to avoid infinite retries
+    const expirationMs = 60000;
+    const eventAgeMs =
+      Date.now() - new Date(userRecord.metadata.creationTime).getTime();
+    if (eventAgeMs > expirationMs) {
+      console.log(
+        `Skipping processing for Firebase user ${userRecord.uid} - creation occurred ${eventAgeMs}ms ago, exceeding ${expirationMs}ms expiration`
+      );
+      return;
+    }
+
+    // Attempt to persist created Firebase user
+    const url = `${AppUrlFactory.getUrl()}/users/writeNewUser`;
+    console.log(
+      `Attempting to persist Firebase user ${userRecord.uid} at ${url}`
+    );
+    try {
+      await axios.post(url, {
+        uid: userRecord.uid,
+      });
+      console.log(`Successfully persisted Firebase user ${userRecord.uid}`);
+    } catch (e: unknown) {
+      console.log(`Error occurred: ${e}`);
+      if (axios.isAxiosError(e)) {
+        // Swallow error if server indicates that user has already been created to avoid retries
+        if (e.status == 409) {
+          console.log(
+            `User ${userRecord.uid} has already been persisted - not attempting retry`
+          );
+          return;
+        }
+      } else {
+        // Otherwise rethrow error to force retry
+        throw e;
+      }
+    }
   });

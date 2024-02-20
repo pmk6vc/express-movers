083d7c9bd0d53b1776b638a17d830ae877f777dd
Varun Kulkarni <varunkulkarni@gmail.com>
2024-02-19 22:56:55 -0500

    initialize app in cf test

--- functions/test/PersistNewFirebaseUser.test.ts
+++ functions/test/PersistNewFirebaseUser.test.ts
@@ -1,21 +1,28 @@
 import { describe, it } from "@jest/globals";
 import * as admin from "firebase-admin";
+import { app } from "firebase-admin";
 import { getAuth } from "firebase-admin/auth";
 import {
   FIREBASE_AUTH_EMULATOR_HOST,
   TEST_GCP_PROJECT_ID,
 } from "../../test/util/TestConstants";
 import { persistFirebaseUserRecordFactory } from "../src/PersistNewFirebaseUser";
+import App = app.App;
 
-process.env.FIREBASE_AUTH_EMULATOR_HOST = FIREBASE_AUTH_EMULATOR_HOST;
-admin.initializeApp({
-  projectId: TEST_GCP_PROJECT_ID,
-});
 describe("persisting new Firebase users should work", () => {
+  let firebaseAdminApp: App;
+
+  beforeAll(async () => {
+    process.env.FIREBASE_AUTH_EMULATOR_HOST = FIREBASE_AUTH_EMULATOR_HOST;
+    firebaseAdminApp = admin.initializeApp({
+      projectId: TEST_GCP_PROJECT_ID,
+    });
+  });
+
   it("should invoke service to persist new Firebase user", async () => {});
 
   it("should ignore expired user creation event", async () => {
-    const x = await getAuth().createUser({
+    const x = await getAuth(firebaseAdminApp).createUser({
       email: "hello@world.com",
     });
     const delay = (ms: number) => {

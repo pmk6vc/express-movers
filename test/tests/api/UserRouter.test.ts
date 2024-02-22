1bcb0d0d690c70cbf4c5235b4f7bb259b0d48f88
Varun Kulkarni <varunkulkarni@gmail.com>
2024-02-22 11:54:38 -0500

    add user router write db tests

--- test/tests/api/UserRouter.test.ts
+++ test/tests/api/UserRouter.test.ts
@@ -6,6 +6,7 @@ import {
   describe,
   it,
 } from "@jest/globals";
+import { eq } from "drizzle-orm";
 import { Express } from "express";
 import { app } from "firebase-admin";
 import { getAuth } from "firebase-admin/auth";
@@ -54,7 +55,7 @@ describe("user routes should work", () => {
 
   const ROUTE_PREFIX = "/users";
 
-  describe("should create new user", () => {
+  describe("should create new user in Firebase", () => {
     it("blocks create request for duplicate user", async () => {
       const res = await request(expressApp)
         .post(ROUTE_PREFIX)
@@ -64,13 +65,126 @@ describe("user routes should work", () => {
     });
 
     it("creates new user", async () => {
+      const fetchUserOne = async () => {
+        return getAuth(firebaseAdminApp).getUserByEmail(TEST_USER_ONE.email);
+      };
+      await expect(fetchUserOne).rejects.toThrow();
+
       const res = await request(expressApp)
         .post(ROUTE_PREFIX)
         .send(TEST_USER_ONE);
       expect(res.status).toBe(201);
       expect(res.text).toBe(`New user ${TEST_USER_ONE.email} created`);
+      expect((await fetchUserOne()).email).toEqual(TEST_USER_ONE.email);
+    });
+  });
+
+  describe("should persist new Firebase user in database", () => {
+    it("blocks request for user that does not exist in Firebase", async () => {
+      const userId = "not-a-real-uid";
+      const res = await request(expressApp)
+        .post(`${ROUTE_PREFIX}/writeNewUser`)
+        .send({
+          uid: userId,
+        });
+      expect(res.status).toBe(409);
+      expect(res.text).toBe(`User ${userId} cannot be found in Firebase`);
     });
 
+    it("blocks database write for duplicate user", async () => {
+      const userId = testUsers[0].userRecord.uid;
+      const res = await request(expressApp)
+        .post(`${ROUTE_PREFIX}/writeNewUser`)
+        .send({
+          uid: userId,
+        });
+      expect(res.status).toBe(409);
+      expect(res.text).toBe(`User ${userId} already exists`);
+    });
+
+    it("persists new Firebase user in database", async () => {
+      const fetchUserOne = async () => {
+        return dbClient.pgPoolClient
+          .select()
+          .from(userTableDef)
+          .where(eq(userTableDef.email, TEST_USER_ONE.email));
+      };
+      const beforeRequest = await fetchUserOne();
+      expect(beforeRequest.length).toBe(0);
+
+      const userOne = await getAuth(firebaseAdminApp).createUser(TEST_USER_ONE);
+      const res = await request(expressApp)
+        .post(`${ROUTE_PREFIX}/writeNewUser`)
+        .send({
+          uid: userOne.uid,
+        });
+      expect(res.status).toBe(201);
+      expect(res.text).toBe(`New user ${TEST_USER_ONE.email} created`);
+
+      const afterRequest = await fetchUserOne();
+      const userOneDb = afterRequest[0];
+      expect(afterRequest.length).toBe(1);
+      expect(userOneDb.uid).toBe(userOne.uid);
+      expect(userOne.email).toBe(TEST_USER_ONE.email);
+    });
+  });
+
+  describe("should get user", () => {
+    it("blocks request with no authenticated user", async () => {
+      const userId = testUsers[0].userRecord.uid;
+      const invalidToken = "not-a-valid-token";
+      const res = await request(expressApp)
+        .get(`${ROUTE_PREFIX}/${userId}`)
+        .set("Authorization", `Bearer ${invalidToken}`);
+      expect(res.status).toBe(401);
+      expect(res.text).toBe("Unauthenticated request");
+    });
+
+    it("blocks request for authenticated user with insufficient permissions", async () => {
+      // Create new users in Firebase and database without any special roles
+      const [userOneFirebase, userTwoFirebase] = await Promise.all([
+        getAuth(firebaseAdminApp).createUser(TEST_USER_ONE),
+        getAuth(firebaseAdminApp).createUser(TEST_USER_TWO),
+      ]);
+      await Promise.all([
+        request(expressApp).post(`${ROUTE_PREFIX}/writeNewUser`).send({
+          uid: userOneFirebase.uid,
+        }),
+        request(expressApp).post(`${ROUTE_PREFIX}/writeNewUser`).send({
+          uid: userTwoFirebase.uid,
+        }),
+      ]);
+
+      // Attempt to fetch user data with token from different user
+      const userTwoToken = await getIdTokenWithEmailPassword(
+        TEST_USER_TWO.email,
+        TEST_USER_TWO.password,
+      );
+      const res = await request(expressApp)
+        .get(`${ROUTE_PREFIX}/${userOneFirebase.uid}`)
+        .set("Authorization", `Bearer ${userTwoToken}`);
+      expect(res.status).toBe(403);
+      expect(res.text).toBe("Unauthorized request");
+    });
+
+    it("returns user data for authenticated user with permissions", async () => {
+      const user = testUsers[0];
+      const bearerToken = await getIdTokenWithEmailPassword(
+        user.userCredentials.email,
+        user.userCredentials.password,
+      );
+      const res = await request(expressApp)
+        .get(`${ROUTE_PREFIX}/${user.userRecord.uid}`)
+        .set("Authorization", `Bearer ${bearerToken}`);
+      expect(res.status).toBe(200);
+      expect(res.body).toMatchObject({
+        uid: user.userRecord.uid,
+        email: user.userCredentials.email,
+      });
+    });
+  });
+
+  describe("should persist user profile", () => {
     // TODO: Add this test for profile update
     // it("persists user data correctly", async () => {
     //   // Create new users with varying levels of profile information
@@ -109,62 +223,5 @@ describe("user routes should work", () => {
     //   expect(testUserTwoRow.email).toBe(testUserTwoFirebase.email);
     //   expect(testUserTwoRow.profile).toEqual(TEST_USER_TWO.profile);
     // });
-  });
-
-  describe("should get user", () => {
-    it("blocks request with no authenticated user", async () => {
-      const userId = testUsers[0].userRecord.uid;
-      const invalidToken = "not-a-valid-token";
-      const res = await request(expressApp)
-        .get(`${ROUTE_PREFIX}/${userId}`)
-        .set("Authorization", `Bearer ${invalidToken}`);
-      expect(res.status).toBe(401);
-      expect(res.text).toBe("Unauthenticated request");
-    });
-
-    it("blocks request for authenticated user with insufficient permissions", async () => {
-      // Create new users in Firebase and database without any special roles
-      const [userOneFirebase, userTwoFirebase] = await Promise.all([
-        getAuth(firebaseAdminApp).createUser(TEST_USER_ONE),
-        getAuth(firebaseAdminApp).createUser(TEST_USER_TWO),
-      ]);
-      await Promise.all([
-        dbClient.pgPoolClient.insert(userTableDef).values({
-          uid: userOneFirebase.uid,
-          email: TEST_USER_ONE.email,
-        }),
-        dbClient.pgPoolClient.insert(userTableDef).values({
-          uid: userTwoFirebase.uid,
-          email: TEST_USER_TWO.email,
-        }),
-      ]);
-
-      // Attempt to fetch user data with token from different user
-      const userTwoToken = await getIdTokenWithEmailPassword(
-        TEST_USER_TWO.email,
-        TEST_USER_TWO.password,
-      );
-      const res = await request(expressApp)
-        .get(`${ROUTE_PREFIX}/${userOneFirebase.uid}`)
-        .set("Authorization", `Bearer ${userTwoToken}`);
-      expect(res.status).toBe(403);
-      expect(res.text).toBe("Unauthorized request");
-    });
-
-    it("returns user data for authenticated user with permissions", async () => {
-      const user = testUsers[0];
-      const bearerToken = await getIdTokenWithEmailPassword(
-        user.userCredentials.email,
-        user.userCredentials.password,
-      );
-      const res = await request(expressApp)
-        .get(`${ROUTE_PREFIX}/${user.userRecord.uid}`)
-        .set("Authorization", `Bearer ${bearerToken}`);
-      expect(res.status).toBe(200);
-      expect(res.body).toMatchObject({
-        uid: user.userRecord.uid,
-        email: user.userCredentials.email,
-      });
-    });
   });
 });
